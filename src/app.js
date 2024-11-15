069dcbfbdaf9ff467a4fe3379b85ed9f74ab31b9
deveshtuteja <deveshtuteja17@gmail.com>
2024-11-16 03:58:07 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 
 // Request Handler
+//Code starts matching routes from top
+//Order of routes matter
+
+app.use("/hello/123", (req, res) => {
+    res.send("ABRACADABRA")
+})
 
 app.use("/hello", (req, res) => {
     res.send("Hello Hello Hello!");
