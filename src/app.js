16bcb92f6ae15be9b518304d5e92937759817b14
deveshtuteja <deveshtuteja17@gmail.com>
2024-11-16 15:05:37 +0530

    exploring get,post, delete

--- src/app.js
+++ src/app.js
@@ -6,20 +6,22 @@ const app = express();
 //Code starts matching routes from top
 //Order of routes matter
 
-app.use("/hello/123", (req, res) => {
-    res.send("ABRACADABRA")
+//this will handle only GET calls to /user
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Devesh", lastName: "Tuteja" })
 })
 
-app.use("/hello", (req, res) => {
-    res.send("Hello Hello Hello!");
+app.post("/user", (req, res) => {
+    res.send("User posted successfully to DB")
 })
 
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully!")
+})
+
+//this will handle all the http methods api call to /test
 app.use("/test", (req, res) => {
     res.send("Testing from the server!");
-})
-
-app.use("/", (req, res) => {
-    res.send("Hello from the server!");
 })
 
 app.listen(7777, () => {
