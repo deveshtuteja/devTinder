c252977f7be2f9cb6973c8c9033792853ce45eb6
deveshtuteja <deveshtuteja17@gmail.com>
2024-11-22 18:04:30 +0530

    error handling- wildcard & trycatch

--- src/app.js
+++ src/app.js
@@ -2,21 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
-//handle auth MIDDLEWARE for all GET,POST.... Requests
+//error handling -> always use try catch
 
-app.use("/admin", adminAuth)
-
-app.get("/admin/getUserData", (req, res) => {
-    res.send("USER DATA FETCHED SUCCESSFULLY!")
+app.get("/getUserData", (req, res) => {
+    try {
+        //Connecting to DB and getting data logic
+        throw new Error("abcdef")
+        res.send("Data fetched successfully")
+    } catch (err) {
+        res.status(500).send("An error has occurred")
+    }
 })
 
-app.get("/admin/deleteUser", (req, res) => {
-    res.send("DATA DELETED SUCCESSFULLY!")
-})
-
-app.get("/user", userAuth, (req, res) => {
-    res.send("User found")
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("Unexpected Error")
+    }
 })
 
 app.listen(7777, () => {
